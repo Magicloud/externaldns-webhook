85fdde0b70854859c735c8251af3ebf09d5e3c96
Magicloud <1886157+Magicloud@users.noreply.github.com>
2025-10-17 17:42:23 +0800

    Rework to follow ExternalDNS changes.
    ExternalDNS started using OpenAPI for this webhook in Nov/2024, which solved a
    few inconsistencies but also introduced a few differences.
    
    Instead of just returning the data, some APIs now accepts `Result` (500 error).
    
    Instead of using default value, unset fields now are "missing".
    
    Instaed of taking various cases of field names, now they must be camel case.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,3 @@
-#![feature(try_blocks)]
-#![feature(let_chains)]
-
-use serde::{Deserialize, Serialize};
-
 pub mod changes;
 pub mod domain_filter;
 pub mod endpoint;
@@ -10,16 +5,6 @@ mod provider;
 mod status;
 mod webhook;
 mod webhook_json;
-
-/// Container of either of the two items.
-/// Rust `Result` works like `Either` in Haskell, but generally implies a good one or
-/// a bad one. There is an `Either` crate, which is even weirder.
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(untagged)]
-enum IDoNotCareWhich<A, B> {
-    One(A),
-    Another(B),
-}
 
 const MEDIATYPE: &str = "application/external.dns.webhook+json;version=1";
 
--- src/webhook.rs
+++ src/webhook.rs
@@ -1,6 +1,6 @@
 use crate::{
-    IDoNotCareWhich, MEDIATYPE, changes::Changes, domain_filter::DomainFilter, endpoint::Endpoint,
-    provider::Provider, status::Status, webhook_json::WebhookJson,
+    MEDIATYPE, domain_filter::DomainFilter, endpoint::Endpoint, provider::Provider, status::Status,
+    webhook_json::WebhookJson,
 };
 use actix_web::{
     App, HttpServer, get,
@@ -11,6 +11,7 @@ use actix_web::{
     web::{Data, Json},
 };
 use logcall::logcall;
+use serde_json::{Value, from_value};
 use std::sync::Arc;
 
 /// Setup of the HTTP server
@@ -47,11 +48,11 @@ impl Webhook {
         let x = self.status.clone();
         let exposed = HttpServer::new(move || {
             App::new()
-                .wrap(Logger::default())
                 .app_data(Data::new(x.clone()))
                 .service(get_healthz)
                 .service(get_metrics)
         })
+        .workers(2)
         .bind((self.exposed_address.clone(), self.exposed_port))?
         .run();
 
@@ -65,6 +66,7 @@ impl Webhook {
                 .service(post_records)
                 .service(post_adjustendpoints)
         })
+        .workers(4)
         .bind((self.provider_address.clone(), self.provider_port))?
         .run();
 
@@ -75,46 +77,65 @@ impl Webhook {
     }
 }
 
-// Negotiate `DomainFilter`
+// Initialisation and negotiates headers and returns domain filter.
+// Returns 200/500
 #[logcall("debug")]
 #[get("/", guard = "media_type_guard")]
 async fn get_root(dns_manager: Data<Arc<dyn Provider>>) -> WebhookJson<DomainFilter> {
     WebhookJson(Json(dns_manager.domain_filter().await))
 }
 
-// Get records
+// Returns the current records.
+// Returns 200/500
 #[logcall("debug")]
 #[get("/records", guard = "media_type_guard")]
 async fn get_records(dns_manager: Data<Arc<dyn Provider>>) -> WebhookJson<Vec<Endpoint>> {
     WebhookJson(Json(dns_manager.records().await))
 }
 
-// Apply record
+// Applies the changes.
+// Returns 204/500
 #[logcall("debug")]
 #[post("/records")]
 async fn post_records(
     dns_manager: Data<Arc<dyn Provider>>,
-    changes: Json<Changes>,
+    changes: Json<Value>,
 ) -> (String, StatusCode) {
-    match dns_manager.apply_changes(changes.0).await {
-        Ok(_) => ("".to_string(), StatusCode::OK),
-        Err(e) => (format!("{e:?}"), StatusCode::INTERNAL_SERVER_ERROR),
+    let json = changes.into_inner();
+    if let Ok(changes) = from_value(json.clone()) {
+        match dns_manager.apply_changes(changes).await {
+            Ok(_) => (String::new(), StatusCode::NO_CONTENT),
+            Err(e) => {
+                log::warn!("{e:?}");
+                (String::new(), StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        }
+    } else {
+        log::warn!("{json}");
+        (String::new(), StatusCode::INTERNAL_SERVER_ERROR)
     }
 }
 
-// Provider specific adjustments of records
+// Executes the AdjustEndpoints method.
+// Returns 200/500
 #[logcall("debug")]
 #[post("/adjustendpoints", guard = "media_type_guard")]
 async fn post_adjustendpoints(
     dns_manager: Data<Arc<dyn Provider>>,
-    endpoints: Json<Vec<Endpoint>>,
-) -> (Json<IDoNotCareWhich<Vec<Endpoint>, String>>, StatusCode) {
-    match dns_manager.adjust_endpoints(endpoints.0).await {
-        Ok(x) => (Json(IDoNotCareWhich::One(x)), StatusCode::OK),
-        Err(e) => (
-            Json(IDoNotCareWhich::Another(format!("{e:?}"))),
-            StatusCode::INTERNAL_SERVER_ERROR,
-        ),
+    endpoints: Json<Value>,
+) -> (Json<Vec<Endpoint>>, StatusCode) {
+    let json = endpoints.into_inner();
+    if let Ok(endpoints) = from_value(json.clone()) {
+        match dns_manager.adjust_endpoints(endpoints).await {
+            Ok(x) => (Json(x), StatusCode::OK),
+            Err(e) => {
+                log::warn!("{e:?}");
+                (Json(vec![]), StatusCode::INTERNAL_SERVER_ERROR)
+            }
+        }
+    } else {
+        log::warn!("{json}");
+        (Json(vec![]), StatusCode::INTERNAL_SERVER_ERROR)
     }
 }
 
